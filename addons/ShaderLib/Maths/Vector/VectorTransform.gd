d3397cf87cb0b6045c342d31c205452b77e0c907
Digvijaysinh Gohil <zdigv96@gmail.com>
2024-01-12 18:21:24 +0530

    VectorTransform node finalized

--- addons/ShaderLib/Maths/Vector/VectorTransform.gd
+++ addons/ShaderLib/Maths/Vector/VectorTransform.gd
@@ -22,13 +22,6 @@ func _get_input_port_name(port: int) -> String:
 func _get_input_port_type(port: int) -> PortType:
 	return PORT_TYPE_VECTOR_3D
 
-func _get_input_port_default_value(port: int) -> Variant:
-	match port:
-		0:
-			return Vector3(0.0, 0.0, 0.0)
-		_:
-			return null
-
 func _get_output_port_count() -> int:
 	return 1
 
@@ -66,13 +59,68 @@ func _get_code(input_vars: Array[String], output_vars: Array[String], mode: Shad
 	var code: String
 	var from_coord_space_index: int = get_option_index(0)
 	var to_coord_space_index: int = get_option_index(1)
+	var input_vector: String = input_vars[0] if input_vars[0] else "vec3(0.0, 0.0, 0.0)"
 
-	#match from_coord_space_index:
-		#0:
-		#1:
-		#2:
-		#3:
-		#4:
+	match from_coord_space_index:
+		0:
+			match to_coord_space_index:
+				0:
+					code = "%s = %s;" % [output_vars[0], input_vector]
+				1:
+					code = "%s = vector_transform_local_to_world(MODEL_MATRIX, %s);" % [output_vars[0], input_vector]
+				2:
+					code = "%s = vector_transform_local_to_view(MODEL_MATRIX, VIEW_MATRIX, %s);" % [output_vars[0], input_vector]
+				3:
+					code = "%s = vector_transform_local_to_screen(MODEL_MATRIX, VIEW_MATRIX, PROJECTION_MATRIX, %s);" % [output_vars[0], input_vector]
+				4:
+					code = "%s = vector_transform_local_to_tangent(NORMAL, BINORMAL, TANGENT, %s);" % [output_vars[0], input_vector]
+		1:
+			match to_coord_space_index:
+				0:
+					code = "%s = vector_transform_world_to_local(MODEL_MATRIX, %s);" % [output_vars[0], input_vector]
+				1:
+					code = "%s = %s;" % [output_vars[0], input_vector]
+				2:
+					code = "%s = vector_transform_world_to_view(VIEW_MATRIX, %s);" % [output_vars[0], input_vector]
+				3:
+					code = "%s = vector_transform_world_to_screen(VIEW_MATRIX, PROJECTION_MATRIX, %s);" % [output_vars[0], input_vector]
+				4:
+					code = "%s = vector_transform_world_to_tangent(MODEL_MATRIX, NORMAL, BINORMAL, TANGENT, %s);" % [output_vars[0], input_vector]
+		2:
+			match to_coord_space_index:
+				0:
+					code = "%s = vector_transform_view_to_local(INV_VIEW_MATRIX, MODEL_MATRIX, %s);" % [output_vars[0], input_vector]
+				1:
+					code = "%s = vector_transform_view_to_world(INV_VIEW_MATRIX, %s);" % [output_vars[0], input_vector]
+				2:
+					code = "%s = %s;" % [output_vars[0], input_vector]
+				3:
+					code = "%s = vector_transform_view_to_screen(PROJECTION_MATRIX, %s);" % [output_vars[0], input_vector]
+				4:
+					code = "%s = vector_transform_view_to_tangent(INV_VIEW_MATRIX, MODEL_MATRIX, NORMAL, BINORMAL, TANGENT, %s);" % [output_vars[0], input_vector]
+		3:
+			match to_coord_space_index:
+				0:
+					code = "%s = vector_transform_screen_to_local(INV_PROJECTION_MATRIX, INV_VIEW_MATRIX, MODEL_MATRIX, %s);" % [output_vars[0], input_vector]
+				1:
+					code = "%s = vector_transform_screen_to_world(INV_PROJECTION_MATRIX, INV_VIEW_MATRIX, %s);" % [output_vars[0], input_vector]
+				2:
+					code = "%s = vector_transform_screen_to_view(INV_PROJECTION_MATRIX, %s);" % [output_vars[0], input_vector]
+				3:
+					code = "%s = %s;" % [output_vars[0], input_vector]
+				4:
+					code = "%s = vector_transform_screen_to_tangent(INV_PROJECTION_MATRIX, INV_VIEW_MATRIX, MODEL_MATRIX, NORMAL, BINORMAL, TANGENT, %s);" % [output_vars[0], input_vector]
+		4:
+			match to_coord_space_index:
+				0:
+					code = "%s = vector_transform_tangent_to_local(NORMAL, BINORMAL, TANGENT, %s);" % [output_vars[0], input_vector]
+				1:
+					code = "%s = vector_transform_tangent_to_world(MODEL_MATRIX, NORMAL, BINORMAL, TANGENT, %s);" % [output_vars[0], input_vector]
+				2:
+					code = "%s = vector_transform_tangent_to_view(MODEL_MATRIX, VIEW_MATRIX, NORMAL, BINORMAL, TANGENT, %s);" % [output_vars[0], input_vector]
+				3:
+					code = "%s = vector_transform_tangent_to_screen(MODEL_MATRIX, VIEW_MATRIX, PROJECTION_MATRIX, NORMAL, BINORMAL, TANGENT, %s);" % [output_vars[0], input_vector]
+				4:
+					code = "%s = %s;" % [output_vars[0], input_vector]
 
-	code = "%s = some_function(%s)" % [input_vars[0], output_vars[0]]
-	return output_vars[0] + " = 0.0;"
+	return code
